522e2399ee290f738de7bd4ba5ba6d514cda330f
anperederi <antoine.perederii@etu.uca.fr>
2023-12-04 17:38:38 +0100

    first commit

---
+++ tp/1_tp/TP1/TP1/Pages/List.razor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+using TP1.Models;
+
+namespace TP1.Pages
+{
+	public partial class List
+	{
+        private Item[] items;
+
+        [Inject]
+        public HttpClient Http { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+        
+        protected override async Task OnInitializedAsync()
+        {
+            items = await Http.GetFromJsonAsync<Item[]>($"{NavigationManager.BaseUri}fake-data.json");
+        }
+        public List()
+		{
+		}
+	}
+}
+
